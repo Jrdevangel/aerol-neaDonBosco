9b9d99010201190dfaac70dc21549d4a98b72323
ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-09-30 11:12:00 +0200

    add

--- src/main/java/com/flightDB/DBApp/model/Routes.java
+++ src/main/java/com/flightDB/DBApp/model/Routes.java
@@ -1,6 +1,8 @@
 package com.flightDB.DBApp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,19 +24,23 @@ public class Routes {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")  // Incluir en la serialización/deserialización
     private Long id;
 
     @Column
+    @JsonProperty("country")  // Incluir en la serialización/deserialización
     private String country;
 
     @Column
+    @JsonProperty("city")  // Incluir en la serialización/deserialización
     private String city;
 
+    // Ignorar estas relaciones en la serialización y deserialización
     @OneToMany(mappedBy = "destination", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonIgnoreProperties("destination")
+    @JsonIgnore  // Ignorar en la serialización
     private Set<Flight> destinationFlights;
 
     @OneToMany(mappedBy = "origin", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonIgnoreProperties("origin")
+    @JsonIgnore  // Ignorar en la serialización
     private Set<Flight> originFlights;
 }
